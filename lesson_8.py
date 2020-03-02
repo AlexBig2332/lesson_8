648d2146d0ee5ba64f72b813f732dce8320d14de
alexbig2332 <alexbig2332@gmail.com>
2020-03-02 14:58:02 +0300

    HW_8

---
+++ lesson_8.py
@@ -0,0 +1,29 @@
+# 1. Написать декоратор, замеряющий время выполнение декорируемой функции.
+# 2. Сравнить время создания генератора и списка с элементами: натуральные числа от 1 до 1000000
+#    (создание объектов оформить в виде функций).
+
+import time
+
+def show_time(f):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        f(*args, **kwargs)
+        finish = time.time()
+        print("Создание списка {} заняло {} секунд".format(f, finish-start))
+    return wrapper
+
+@show_time
+def list_create_gen(N):
+    list_gen = [i for i in range(1, N+1)]
+    return list_gen
+
+@show_time
+def list_create(N):
+    list = []
+    for i in range(N):
+        list.append(i+1)
+    return list
+
+list_create_gen(1000000)
+list_create(1000000)
+
