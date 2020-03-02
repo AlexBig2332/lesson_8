61a72c71fe5974752bf3cbba129cc05a75970c04
alexbig2332 <alexbig2332@gmail.com>
2020-03-02 16:40:18 +0300

    HW_8

--- lesson_8.py
+++ lesson_8.py
@@ -3,27 +3,42 @@
 #    (создание объектов оформить в виде функций).
 
 import time
+import os
+import psutil
 
 def show_time(f):
     def wrapper(*args, **kwargs):
+        proc = psutil.Process(os.getpid())
+        print('Исп. память до вып. функции:' + str(proc.memory_info().rss / 1000000))
         start = time.time()
         f(*args, **kwargs)
-        finish = time.time()
-        print("Создание списка {} заняло {:.5} секунд".format(f, finish - start))
+        # print(f(*args, **kwargs))
+        print('Исп. память после вып. функции:' + str(proc.memory_info().rss / 1000000))
+        stop = time.time()
+        print("Выполнение {} заняло {:.5} секунд".format(f, stop - start))
+        print()
     return wrapper
 
+N = 1000000
 @show_time
 def list_create_gen(N):
+    for i in range(N):
+         yield (i)
+
+@show_time
+def list_create_gen_sequence(N): # Создание списка генератором последовательностей
     list_gen = [i for i in range(1, N+1)]
     return list_gen
 
 @show_time
-def list_create(N):
+def list_create(N): # Создание списка в цикле
     list = []
     for i in range(N):
         list.append(i+1)
     return list
 
-list_create_gen(1000000)
-list_create(1000000)
+
+list_create_gen(N)
+list_create_gen_sequence(N)
+list_create(N)
 
